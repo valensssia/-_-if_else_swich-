9962feac13b6a4500be78253e808a6dcd7b8536d
valensssia <99199656+valensssia@users.noreply.github.com>
2022-02-23 20:50:35 +0200

    Add files via upload

---
+++ reshta.java
@@ -0,0 +1,18 @@
+package if_else_switch;
+
+import java.util.Scanner;
+
+public class reshta {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		int e = (c * 100 + d - a * 100 - b)/ 100;
+		int f = (c * 100 + d - a * 100 - b) % 100;
+		System.out.println(e + " " + f);
+	}
+
+
+}
