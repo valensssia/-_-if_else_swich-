07260c1590d64094c1aaa6353a9d579c19b1d473
valensssia <99199656+valensssia@users.noreply.github.com>
2022-02-23 20:26:55 +0200

    Add files via upload

---
+++ znak_chysla.java
@@ -0,0 +1,21 @@
+package if_else_switch;
+
+import java.util.Scanner;
+
+public class znak_chysla {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		if (x > 0) {
+			System.out.println("1");
+		}else {
+			if(x == 0) {
+				System.out.println("0");
+			}else {
+				System.out.println("-1");
+			}
+			}
+		}
+		}
+
+
