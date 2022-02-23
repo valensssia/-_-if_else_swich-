72ea538b7cd5ea11afa5fb94c8e4e8f0ac38c041
valensssia <99199656+valensssia@users.noreply.github.com>
2022-02-23 20:45:09 +0200

    Add files via upload

---
+++ fishky.java
@@ -0,0 +1,17 @@
+package if_else_switch;
+
+import java.util.Scanner;
+
+public class fishky {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int k = sc.nextInt();
+		if (k % 4 == 0) {
+			System.out.println("Yes");
+		}else {
+			System.out.println("No");
+		}
+		}
+		
+
+}
