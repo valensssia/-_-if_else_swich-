f93ec50d452f90e69d51cffba52d0248d5412973
valensssia <99199656+valensssia@users.noreply.github.com>
2022-02-23 20:20:09 +0200

    Add files via upload

---
+++ maximum_is_dvoch_chisel.java
@@ -0,0 +1,19 @@
+package if_else_switch;
+
+import java.util.Scanner;
+
+public class maximum_is_dvoch_chisel {
+	public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	int a = sc.nextInt();
+	int b = sc.nextInt();
+	if (a > b) {
+	    System.out.println(a);
+	}else{
+		System.out.println(b);
+	}
+}
+}
+	
+
+
