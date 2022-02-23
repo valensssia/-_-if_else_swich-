346fdfc8d1707933388647a8aa2a248ad93578ae
valensssia <99199656+valensssia@users.noreply.github.com>
2022-02-23 20:34:03 +0200

    Add files via upload

---
+++ ladya.java
@@ -0,0 +1,20 @@
+package if_else_switch;
+
+import java.util.Scanner;
+
+public class ladya {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int a1 = sc.nextInt();
+		int a2 = sc.nextInt();
+		int b1 = sc.nextInt();
+		int b2 = sc.nextInt();
+		if ((a1 == b1) || (a2 == b2)){
+			System.out.println("Yes");
+			}else{
+				System.out.println("No");
+				}
+			}
+	}
+
+
